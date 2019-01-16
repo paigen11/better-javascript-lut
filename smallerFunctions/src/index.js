f567712df26ad73ceb0515a5a13e16106675db91
Paige Niedringhaus <paigen11@gmail.com>
2019-01-16 16:28:15 -0500

    breaking down functions into pure and small

--- smallerFunctions/src/index.js
+++ smallerFunctions/src/index.js
@@ -6,23 +6,31 @@ document.getElementById('app').innerHTML = `
   for more info about Parcel.
 </div>
 `;
+// constant variables
+const SHIPPING_COST = 10;
 
 const cart = ['10', '5', '15'];
 
 const fakeAPICharge = total => true;
 const fakeSendRecipt = total => true;
 
+// pure functions are smaller and single responsibility
+// (which makes them more individually testable)
 const getSubTotal = cart => cart.reduce((tempTotal, item) => tempTotal + item);
+const getTotal = subTotal => subTotal + SHIPPING_COST;
+const sendReceipt = ({ email, total }) =>
+  fakeSendReceipt({
+    email,
+    total,
+  });
 
+// makes this checkout function more readable
 const checkout = cart => {
   const subTotal = getSubTotal(cart);
-  const total = subTotal + 10;
+  const total = getTotal(subTotal);
   const orderSuccess = fakeAPICharge(total);
   if (orderSuccess) {
-    fakeSendRecipt({
-      email: 'fakeemail@gmail.com',
-      total,
-    });
+    sendReceipt({ email: 'fakeemail@gmail.com', total });
   }
   return orderSuccess;
 };

a2a572bb75af1b3747716f1a742be7c80d926b76
Paige Niedringhaus <paigen11@gmail.com>
2019-01-21 16:42:58 -0500

    more dom work

--- jsDom/src/index.js
+++ jsDom/src/index.js
@@ -1,11 +1,5 @@
-document.getElementById('app').innerHTML = `
-<h1>Hello Parcel!</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
-`;
+const logo = document.getElementById('logo');
+logo.innerText = 'YO!';
 
 // doc resource https://developer.mozilla.org/en-US/
 
